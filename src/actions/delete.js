4d0a4d0f1e8f0828c2a5892d0ab43eb4203d37f9
scanf6 <scanf555@gmail.com>
2021-01-19 15:14:18 +0300

    fix: added action before dispatching deletions

--- src/actions/delete.js
+++ src/actions/delete.js
@@ -1,15 +1,21 @@
 import {
-  SECTION_ERASE_ALL, STIMULATION_ERASE_ALL, DELETE_BULBAR, DELETE_MEDULAR,
+  SECTION_ERASE_ALL,
+  STIMULATION_ERASE_ALL,
+  SET_STIMULATION_OR_SECTION,
+  DELETE_BULBAR,
+  DELETE_MEDULAR,
 } from '../types';
 
 export const deleteBulbar = () => (dispatch) => {
   dispatch({ type: SECTION_ERASE_ALL });
   dispatch({ type: STIMULATION_ERASE_ALL });
+  dispatch({ type: SET_STIMULATION_OR_SECTION });
   dispatch({ type: DELETE_BULBAR });
 };
 
 export const deleteMedular = () => (dispatch) => {
   dispatch({ type: SECTION_ERASE_ALL });
   dispatch({ type: STIMULATION_ERASE_ALL });
+  dispatch({ type: SET_STIMULATION_OR_SECTION });
   dispatch({ type: DELETE_MEDULAR });
 };
